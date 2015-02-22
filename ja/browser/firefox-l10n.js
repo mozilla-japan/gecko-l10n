3575c6b147747d812b9978666c454b833e6785b5
Kazuyuki SUZUKI <kechol@users.noreply.github.com>
2015-02-22 16:42:28 +0900

    37.0: sync mobile with en-US rev240711:3c1a3493ac00

---
+++ ja/browser/firefox-l10n.js
@@ -0,0 +1,15 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#filter substitution
+
+pref("general.useragent.locale", "@AB_CD@");
+
+pref("browser.search.order.3", "chrome://browser-region/locale/region.properties");
+pref("browser.search.order.4", "chrome://browser-region/locale/region.properties");
+pref("browser.search.order.5", "chrome://browser-region/locale/region.properties");
+pref("browser.search.order.6", "chrome://browser-region/locale/region.properties");
+pref("browser.search.order.7", "chrome://browser-region/locale/region.properties");
+pref("browser.search.order.8", "chrome://browser-region/locale/region.properties");
+pref("browser.search.order.9", "chrome://browser-region/locale/region.properties");
